e2c997bc7d36a2eea27d65fd7e1cb49c2c3ce93a
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-07 18:21:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-my_hash {}
+my_hash = {"orange" => "tasty fruit", "moby dick" => "a great novel"}
 
 end
 
